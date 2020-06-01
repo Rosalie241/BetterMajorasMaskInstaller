943e0bdf33a784581a543456ab7f96379c00f286
Tim <tim241@mailbox.org>
2020-06-01 20:04:17 +0200

    build_release.bat: generate MD5 hash

--- build_release.bat
+++ build_release.bat
@@ -6,3 +6,6 @@ msbuild /t:Clean /t:Build /t:ILMerge ^
 	/p:Configuration=Release ^
 	/p:AllowedReferenceRelatedFileExtensions=none ^
 	BetterMajorasMaskInstaller\BetterMajorasMaskInstaller.csproj
+
+:: Generate MD5 Hash
+CertUtil -hashfile bin\BetterMajorasMaskInstaller.exe MD5
